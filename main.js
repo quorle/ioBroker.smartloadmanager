619d1ea4d36169fa895e82b4f4fca9c4c87cc8e0
quorle <38273154+quorle@users.noreply.github.com>
2025-06-27 03:44:33 +0200

    Save main.js.

--- main.js
+++ main.js
@@ -1,165 +1,108 @@
-"use strict";
+'use strict';
 
-/*
- * Created with @iobroker/create-adapter v2.6.5
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
-const utils = require("@iobroker/adapter-core");
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const utils = require('@iobroker/adapter-core');
 
 class Nulleinspeisung extends utils.Adapter {
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "nulleinspeisung",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
+    constructor(options = {}) {
+        super({ ...options, name: 'nulleinspeisung' });
+    }
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
+    async onReady() {
+        const einspeisungId = this.config.einspeisungId;
+        if (!einspeisungId) {
+            this.log.error('Einspeisungs-Datenpunkt nicht konfigiert.');
+            return;
+        }
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+        this.subscribeForeignStates(einspeisungId);
+        this.log.info(`Beobachte: ${einspeisungId}`);
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+        const state = await this.getForeignStateAsync(einspeisungId);
+        if (state && typeof state.val === 'number') {
+            await this.regleVerbraucher(state.val);
+        }
+    }
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+    async onStateChange(id, state) {
+        if (!state || state.ack !== true) return;
+        if (id === this.config.einspeisungId && typeof state.val === 'number') {
+            await this.regleVerbraucher(state.val);
+        }
+    }
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+    async regleVerbraucher(einspeisung) {
+        const verbraucher = this.config.verbraucher || [];
+        const zustand = {};
+        let freieLeistung = einspeisung > 0 ? einspeisung : 0;
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+        if (einspeisung >= 0) {
+            for (const v of verbraucher) {
+                const { datenpunkt: dp, leistung, name, regelTyp = 'binary', vollzuschaltung = false } = v;
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+                if (regelTyp === 'percent') {
+                    const prozent = vollzuschaltung
+                        ? (freieLeistung >= leistung ? 100 : 0)
+                        : Math.min(100, Math.floor((freieLeistung / leistung) * 100));
+                    zustand[dp] = prozent;
+                    freieLeistung -= (prozent / 100) * leistung;
+                    this.log.debug(`${name}: auf ${prozent}% geregelt`);
+                } else {
+                    if (freieLeistung >= leistung) {
+                        zustand[dp] = true;
+                        freieLeistung -= leistung;
+                        this.log.debug(`${name}: eingeschaltet`);
+                    } else {
+                        zustand[dp] = false;
+                        this.log.debug(`${name}: nicht genug Leistung`);
+                    }
+                }
+            }
+        } else {
+            let bezug = Math.abs(einspeisung);
+            for (const v of [...verbraucher].reverse()) {
+                const { datenpunkt: dp, leistung, name, regelTyp = 'binary' } = v;
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+                if (regelTyp === 'percent') {
+                    const state = await this.getForeignStateAsync(dp);
+                    const alt = state?.val || 0;
+                    const aktuellWatt = (alt / 100) * leistung;
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
-	}
+                    if (aktuellWatt > bezug) {
+                        const neu = Math.max(0, Math.floor(((aktuellWatt - bezug) / leistung) * 100));
+                        zustand[dp] = neu;
+                        bezug = 0;
+                    } else {
+                        zustand[dp] = 0;
+                        bezug -= aktuellWatt;
+                    }
+                } else {
+                    const state = await this.getForeignStateAsync(dp);
+                    if (state?.val === true) {
+                        zustand[dp] = false;
+                        bezug -= leistung;
+                    }
+                }
 
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
+                if (bezug <= 0) break;
+            }
+        }
 
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
+        for (const [dp, wert] of Object.entries(zustand)) {
+            try {
+                const alt = await this.getForeignStateAsync(dp);
+                if (alt?.val !== wert) {
+                    await this.setForeignStateAsync(dp, wert, true);
+                    this.log.info(`${dp} → ${wert}`);
+                }
+            } catch (err) {
+                this.log.warn(`Fehler beim Setzen von ${dp}: ${err.message}`);
+            }
+        }
+    }
 }
 
 if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new Nulleinspeisung(options);
+    module.exports = (options) => new Nulleinspeisung(options);
 } else {
-	// otherwise start the instance directly
-	new Nulleinspeisung();
+    new Nulleinspeisung();
 }
